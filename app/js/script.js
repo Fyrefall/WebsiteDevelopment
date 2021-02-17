a4ff70949bdece9dd369f03e0f0ee39cca801610
Jonathan <jfenn-tye@live.co.uk>
2021-02-17 10:27:28 +0000

    Landing page complete

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,28 @@
-console.log('Hello World');
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click',function(){
+    console.log('open hamburger')
+
+    if(header.classList.contains('open')){//close menu
+        body.classList.remove('stopscroll');
+        header.classList.remove('open');
+        fadeElements.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    }
+    else{//open menu
+        body.classList.add('stopscroll');
+        header.classList.add('open');
+        fadeElements.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+        
+
+    }
+});
