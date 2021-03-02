f7dfa5cf0ef616eec9c93dcd6b533d6c7a277993
Jonathan <jfenn-tye@live.co.uk>
2021-03-02 20:09:45 +0000

    General website completed

--- app/js/script.js
+++ app/js/script.js
@@ -3,6 +3,8 @@ const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElements = document.querySelectorAll('.has-fade');
+const accordion = document.querySelectorAll('.acc__accordion');
+
 
 btnHamburger.addEventListener('click',function(){
     console.log('open hamburger')
@@ -26,3 +28,20 @@ btnHamburger.addEventListener('click',function(){
 
     }
 });
+
+var acc = document.getElementsByClassName("acc__accordion");
+var i;
+
+for(i=0; i< acc.length; i++){
+    acc[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+
+        if(panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        }
+        else{
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
